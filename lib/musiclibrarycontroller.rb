e8506be5912c95c47d6ffdc62eeaf3ae65a5ae1f
Kyle Luke <th.oldschool@gmail.com>
2019-09-27 23:13:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -78,6 +78,8 @@ class MusicLibraryController
     if (1..songs.length).include?(num)
       play = songs[num +2]
       puts "Playing #{play.name} by #{play.artist.name}"
+    else
+      puts "Song not included in library."
     end
   end
 end
