e90ba148e85e750b10276d26c270c9f858c0ac3f
Kyle Luke <th.oldschool@gmail.com>
2019-09-27 23:26:50 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -78,7 +78,7 @@ class MusicLibraryController
     sorted = Song.all.sort_by{|song| song.name}
     play = sorted[num + 2]
     p play.name
-    if (1..sorted.length).include?(num)
+    if (2..sorted.length).include?(num)
       puts "Playing #{play.name} by #{play.artist.name}"
     end
   end
