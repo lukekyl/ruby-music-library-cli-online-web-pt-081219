7970ee3e420af03111fd7a61a7005597d1b8031a
Kyle Luke <th.oldschool@gmail.com>
2019-09-27 23:44:16 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -75,7 +75,7 @@ class MusicLibraryController
     songs = Song.all
     input = gets.chomp
     num = input.to_i
-    if (1..songs.length).include?(num)
+    if input > 0 && input <= Song.all.length
       sorted = songs.sort_by{|song| song.name}
       play = sorted[num - 1]
       p play.name
