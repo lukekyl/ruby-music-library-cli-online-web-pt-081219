51fa4962be3108daf60ec8c1c329069ea672f086
Kyle Luke <th.oldschool@gmail.com>
2019-09-27 23:48:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  extend Concerns::Findable
   attr_accessor :name
   attr_reader :artist, :genre
   @@all = []
@@ -34,12 +35,12 @@ class Song
   def self.destroy_all
     @@all.clear
   end
-  def self.find_by_name(name)
-    self.all.detect{|song| song.name == name}
-  end
-  def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create(name)
-  end
+  # def self.find_by_name(name)
+  #   self.all.detect{|song| song.name == name}
+  # end
+  # def self.find_or_create_by_name(name)
+  #     self.find_by_name(name) || self.create(name)
+  # end
   def self.new_from_filename(file)
     separated = file.chomp(".mp3").split(" - ")
     new_song = self.find_or_create_by_name(separated[1])
