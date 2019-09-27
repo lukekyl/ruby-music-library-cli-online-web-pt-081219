a425b9992e6937e2ee16f72fc52cd8a6d225435a
Kyle Luke <th.oldschool@gmail.com>
2019-09-27 23:47:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :name
-  attr_reader :artist, :genre, :musicimporter, :musiclibrarycontroller
+  attr_reader :artist, :genre, 
   @@all = []
   def initialize(name, artist = nil, genre = nil)
     self.name = name
